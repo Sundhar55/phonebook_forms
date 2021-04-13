a232421077b65f8031eff692333845e1b22ee94b
Sundhar K <sundhar.karthikeyan@chubb.com>
2021-04-13 18:00:20 +0800

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function PhoneBookForm(){
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+
+  const HandleChange =(e) => {
+    console.log('change', e.target.value);
+  }
+  const onSubmit = (e) => {
+    console.log('submit')
+  }
+  return(
+    <form onSubmit={(e)=>onSubmit(e)}>
+      <input type='text' placeholder='first name' onChange={(e)=> HandleChange(e)}></input>
+      <input type='text' placeholder='last name'  onChange={(e)=> HandleChange(e)}></input>
+      <input type='button' value='Submit' onSubmit={(e)=>onSubmit(e)}></input>
+    </form>
+    
+  )
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-Container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        PhoneBook Form        
       </header>
+      <PhoneBookForm/>
     </div>
+    
   );
 }
 
